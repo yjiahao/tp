f0258dcb8d2fb544d87c34276a988124b9dced77
Yi Fei <e1281068@u.nus.edu>
2026-03-26 14:22:37 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/logic/parser/CopyCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CopyCommandParserTest.java
@@ -1,11 +1,11 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIds.ID_FIRST;
 
 import org.junit.jupiter.api.Test;
