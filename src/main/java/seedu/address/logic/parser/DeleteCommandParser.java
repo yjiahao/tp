25b848c2f4c800b8a0ad42a89c905dd329f2747b
microaugust <203528273+microaugust@users.noreply.github.com>
2026-04-02 16:14:58 +0800

    Change Import Order in DeleteCommandParser
    Improve coding standard by moving the import static to the top of the
    import list.

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.parser;
 
-import java.util.ArrayList;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.ArrayList;
 
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
