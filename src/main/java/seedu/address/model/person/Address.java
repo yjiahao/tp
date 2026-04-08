95635210dd9fc43c45b525906580a80eb49da6b6
yjiahao <yjiahao123@gmail.com>
2026-04-08 17:38:07 +0800

    fix: Check address cannot contain forward slash

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -9,9 +9,10 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, "
+        + "should not contain /, and it should not be blank";
 
-    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public static final String VALIDATION_REGEX = "[^\\s/][^/]*";
 
     public final String value;
 
